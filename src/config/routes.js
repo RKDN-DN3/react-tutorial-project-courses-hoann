1c3ce5443b1c795ae167abd27fcd928cdb87dc2e
HoaNN <hoann@rikkeisoft.com>
2022-08-24 11:35:57 +0700

    Done reviews

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,11 +2,8 @@ const routers = {
     home: '/',
     birthdate: '/birthdate',
     tours: '/tours',
-<<<<<<< HEAD
     reviews: '/reviews',
-=======
     accordion: '/accordion',
->>>>>>> ed5c2f2164f387bd6f49aed7795c64c2346f1006
 };
 
 export default routers;
--- src/projects/reviews/index.js
+++ src/projects/reviews/index.js
@@ -1,4 +1,5 @@
 import classNames from 'classnames/bind';
+import ReactLoading from 'react-loading';
 
 import styles from './reviews.module.scss';
 import unnamed from '../../assets/images/unnamed.png';
@@ -11,43 +12,95 @@ function Reviews() {
     const [id, setId] = useState(0);
     const [lstUser, setLstUser] = useState([]);
     const [user, setUser] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        const fetchData = async () => {
-            const data = await fetch('https://jsonplaceholder.typicode.com/users');
-            const json = await data.json();
-            setLstUser(json);
-            setUser(lstUser[0]);
-        };
-        fetchData();
+        fetch('https://jsonplaceholder.typicode.com/comments')
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                }
+            })
+            .then((users) => {
+                setLstUser(users.slice(0, 10));
+                setUser(users[id]);
+                setIsLoading(false);
+            });
     }, []);
+
+    const handleNextPrevious = (isNext = false) => {
+        if (isNext) {
+            if (id === lstUser.length - 1) {
+                setUser(lstUser[0]);
+                setId(0);
+            } else {
+                setUser(lstUser[id + 1]);
+                setId((pre) => {
+                    return pre + 1;
+                });
+            }
+        } else {
+            if (id === 0) {
+                setUser(lstUser[lstUser.length - 1]);
+                setId(lstUser.length - 1);
+            } else {
+                setUser(lstUser[id - 1]);
+                setId((pre) => {
+                    return pre - 1;
+                });
+            }
+        }
+    };
     return (
         <div className={cx('wrapper')}>
-            <span className={cx('main-title')}>Our Reviews</span>
-            <div className={cx('underline')}></div>
+            {isLoading ? (
+                <ReactLoading type="spinningBubbles" color="#49a6e9" />
+            ) : (
+                <>
+                    <span className={cx('main-title')}>Our Reviews</span>
+                    <div className={cx('underline')}></div>
 
-            <section className={cx('content')}>
-                <div className={cx('avatar')}>
-                    <img className={cx('image')} alt="" src={unnamed}></img>
-                    <div className={cx('round')}></div>
-                    <div className={cx('quote')}>
-                        <FaQuoteRight className={cx('')} color="white"></FaQuoteRight>
-                    </div>
-                </div>
-                <span className={cx('name')}>{user?.name}</span>
-                <span className={cx('work')}>{user?.company?.name}</span>
-                <span className={cx('intro')}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum ex ligula, et mollis justo
-                    aliquam vitae. Duis aliquam dolor leo, quis lacinia purus sagittis sed
-                </span>
-                <div className={cx('action')}>
-                    <FaAngleLeft className={cx('left')} color="#49a6e9" size={'2em'}></FaAngleLeft>
-                    <FaAngleRight className={cx('right')} color="#49a6e9" size={'2em'}></FaAngleRight>
-                </div>
-                <div>
-                    <button className={cx('random')}>Surprise Me</button>
-                </div>
-            </section>
+                    <section className={cx('content')}>
+                        <div className={cx('avatar')}>
+                            <img className={cx('image')} alt="" src={unnamed}></img>
+                            <div className={cx('round')}></div>
+                            <div className={cx('quote')}>
+                                <FaQuoteRight className={cx('')} color="white"></FaQuoteRight>
+                            </div>
+                        </div>
+                        <span className={cx('name')}>{user.email}</span>
+                        <span className={cx('work')}>{user.name}</span>
+                        <span className={cx('intro')}>{user.body}</span>
+
+                        <div className={cx('action')}>
+                            <FaAngleLeft
+                                className={cx('left')}
+                                color="#49a6e9"
+                                size={'2em'}
+                                onClick={() => handleNextPrevious()}
+                            ></FaAngleLeft>
+                            <FaAngleRight
+                                className={cx('right')}
+                                color="#49a6e9"
+                                size={'2em'}
+                                onClick={() => handleNextPrevious(true)}
+                            ></FaAngleRight>
+                        </div>
+                        <div>
+                            <button
+                                className={cx('random')}
+                                onClick={() => {
+                                    let index = Math.floor(Math.random() * 10);
+                                    setUser(lstUser[index]);
+                                    setId(index);
+                                }}
+                            >
+                                Surprise Me
+                            </button>
+                        </div>
+                    </section>
+                </>
+            )}
         </div>
     );
 }
