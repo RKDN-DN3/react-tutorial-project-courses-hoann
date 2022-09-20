2ee9b6245a6aae563403d638c1f0ac80a3ee3d58
Ngoc Hoa <hoann@rikkeisoft.com>
2022-09-20 21:03:34 +0700

    Done Stock Photos

--- src/projects/stock/index.js
+++ src/projects/stock/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import classnames from 'classnames/bind';
 import { FaSearch } from 'react-icons/fa';
 import axios from 'axios';
@@ -9,17 +9,75 @@ import Photo from './Photo';
 const cx = classnames.bind(styles);
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
-const clientId = 'f7N-c7ynV9x6FAE3c1mP35-_1uRQeFNKMYlRro55XGA';
+const clientId = '?client_id=2LTqHddj40esyqcF-4ti8tj8lLu5XX27FpWLIRPyqGg';
 
 function StockPhoto() {
     const [query, setQuery] = useState('');
+    const [page, setPage] = useState(1);
     const [photos, setPhotos] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [newImages, setNewImages] = useState(false);
+    const mounted = useRef(false);
 
     const fetchData = async () => {
         setLoading(true);
-        const res = await axios();
+        let url;
+        const urlPage = `&page=${page}`;
+        const urlQuery = `&query=${query}`;
+        if (query) {
+            url = searchUrl + clientId + urlPage + urlQuery;
+        } else {
+            url = mainUrl + clientId + urlPage;
+        }
+
+        const res = await (await axios(url)).data;
+        setPhotos((oldPhotos) => {
+            if (query && page === 1) {
+                return res.results;
+            } else if (query) {
+                return [...oldPhotos, ...res.results];
+            } else {
+                return [...oldPhotos, ...res];
+            }
+        });
+        setLoading(false);
+        setNewImages(false);
     };
+
+    useEffect(() => {
+        fetchData();
+    }, [page]);
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        if (!query) return;
+        if (page === 1) {
+            fetchData();
+        }
+        setPage(1);
+    };
+
+    const event = () => {
+        if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
+            setNewImages(true);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', event);
+        return () => window.removeEventListener('scroll', event);
+    }, []);
+
+    useEffect(() => {
+        if (!mounted.current) {
+            mounted.current = true;
+            return;
+        }
+        if (!newImages) return;
+        if (loading) return;
+        setPage((oldPage) => oldPage + 1);
+    }, [newImages]);
+
     return (
         <div>
             <section className={cx('search')}>
@@ -31,7 +89,7 @@ function StockPhoto() {
                         onChange={(e) => setQuery(e.target.value)}
                         className={cx('form-input')}
                     />
-                    <button type="submit" className={cx('submit-btn')}>
+                    <button type="submit" className={cx('submit-btn')} onClick={handleSearch}>
                         <FaSearch />
                     </button>
                 </form>
